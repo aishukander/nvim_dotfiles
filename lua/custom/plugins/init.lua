270c35014d2e5aa7d18656488a9debbd00dfe2e4
aishukander <hank960924@gmail.com>
2026-01-29 21:42:28 +0800

    add bufferline and flash

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,30 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'folke/flash.nvim',
+    opts = {},
+    keys = {
+      { 's', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
+      { 'S', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
+    },
+  },
+
+  {
+    'akinsho/bufferline.nvim',
+    event = 'VeryLazy',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = {
+      options = {
+        mode = 'buffers',
+        separator_style = 'slant', 
+        diagnostics = 'nvim_lsp',
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        always_show_bufferline = false,
+      },
+    },
+  },
+}

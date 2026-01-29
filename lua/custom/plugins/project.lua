7012c9e424029976e1a2dd50df739405f4d9aba4
aishukander <hank960924@gmail.com>
2026-01-29 21:49:26 +0800

    add project botton to home menu

---
+++ lua/custom/plugins/project.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    'ahmedkhalf/project.nvim',
+    event = 'VimEnter',
+    config = function()
+      require('project_nvim').setup {
+        -- Manual mode doesn't automatically change your root directory, so you have
+        -- the option to manually do so using `:ProjectRoot` command.
+        manual_mode = false,
+        -- Methods of detecting the root directory. **"lsp"** uses the native neovim
+        -- lsp, while **"pattern"** uses vim-rooter like glob patterns. The order
+        -- matters: if one is not detected, the other is used as fallback. You can
+        -- also delete or rearange the detection methods.
+        detection_methods = { 'lsp', 'pattern' },
+        -- All the patterns used to detect root dir, when **"pattern"** is in
+        -- detection_methods
+        patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json' },
+      }
+      require('telescope').load_extension 'projects'
+    end,
+  },
+}

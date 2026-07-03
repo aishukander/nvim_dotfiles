d8d1c72c9fd65970d76dc7510d44ebc2da8fa833
aishukander <hank960924@gmail.com>
2026-07-03 21:51:09 +0800

    copilot alt+l to accept

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -31,7 +31,7 @@ return {
           enabled = true,
           auto_trigger = true,
           keymap = {
-            accept = false, -- Handled by blink.cmp <Tab>
+            accept = '<M-l>',
             next = '<M-]>',
             prev = '<M-[>',
             dismiss = '<C-]>',

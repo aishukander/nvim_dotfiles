ef9b6c324a5b9b45ad1daaf053c75f25fc9ed21f
aishukander <hank960924@gmail.com>
2026-08-08 14:02:50 +0800

    mark keymaps

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -1,6 +1,7 @@
 -- Put your custom keymaps here.
 -- This file is loaded automatically after startup, so it stays separate from init.lua.
 
+-- Buffer management keymaps
 vim.keymap.set('n', '<leader>bd', function()
   require('mini.bufremove').delete(0, false)
 end, { desc = 'Delete Buffer' })

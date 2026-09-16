7e36b8f8e465e7f6badf8c62fff45861ad807f08
aishukander <hank960924@gmail.com>
2026-09-16 20:30:35 +0800

    one lazygit and one buf one terminal

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -3,7 +3,8 @@ vim.schedule(function()
 end)
 
 local loaded = false
-local terminal
+local buf_terminals = {}
+local lazygit = nil
 
 local function load_toggleterm()
   if loaded then return end
@@ -19,21 +20,19 @@ local function load_toggleterm()
   }
 end
 
-local function toggle_toggleterm_at_pwd()
+local function toggle_toggleterm_at_buf()
   load_toggleterm()
 
-  local dir = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':p:h')
-
-  if dir == '' or vim.fn.isdirectory(dir) == 0 then
-    dir = vim.fn.getcwd()
-  end
-
-  if terminal and terminal.dir ~= dir then
-    terminal:shutdown()
-    terminal = nil
-  end
+  local bufnr = vim.api.nvim_get_current_buf()
+  local terminal = buf_terminals[bufnr]
 
   if not terminal then
+    local dir = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':p:h')
+
+    if dir == '' or vim.fn.isdirectory(dir) == 0 then
+      dir = vim.fn.getcwd()
+    end
+
     local Terminal = require('toggleterm.terminal').Terminal
     terminal = Terminal:new {
       dir = dir,
@@ -42,31 +41,45 @@ local function toggle_toggleterm_at_pwd()
         border = 'curved',
       },
     }
+    buf_terminals[bufnr] = terminal
   end
 
   terminal:toggle()
 end
 
-vim.keymap.set('n', '<leader>rt', toggle_toggleterm_at_pwd, { desc = 'Open Terminal' })
-vim.keymap.set('n', '<c-\\>', toggle_toggleterm_at_pwd, { desc = 'Toggle Terminal' })
-vim.keymap.set('t', '<c-\\>', function () terminal:toggle() end, { desc = 'Toggle Terminal' })
+vim.api.nvim_create_autocmd('BufWipeout', {
+  group = vim.api.nvim_create_augroup('ToggleTermBufferClean', { clear = true }),
+  callback = function(args)
+    local bufnr = args.buf
+    if buf_terminals[bufnr] then
+      buf_terminals[bufnr]:shutdown()
+      buf_terminals[bufnr] = nil
+    end
+  end,
+})
+
+vim.keymap.set('n', '<c-\\>', toggle_toggleterm_at_buf, { desc = 'Toggle Terminal' })
+vim.keymap.set('t', '<c-\\>', function() vim.cmd('close') end, { desc = 'Toggle Terminal', silent = true })
 
 vim.keymap.set('n', '<leader>g', function()
   load_toggleterm()
 
   if vim.fn.executable 'lazygit' ~= 1 then
-    vim.schedule(function()
-      vim.notify('lazygit not found. Please install it', vim.log.levels.ERROR)
-    end)
+    vim.notify('lazygit not found. Please install it', vim.log.levels.ERROR)
+    return
+  end
+
+  if lazygit and lazygit:is_open() then
+    lazygit:close()
     return
   end
 
   local current_file = vim.api.nvim_buf_get_name(0)
-  local search_path = current_file ~= "" and vim.fn.fnamemodify(current_file, ":p:h") or vim.fn.getcwd()
+  local search_path = current_file ~= '' and vim.fn.fnamemodify(current_file, ':p:h') or vim.fn.getcwd()
 
-  local git_path = vim.fs.find('.git', { 
-    upward = true, 
-    path = search_path 
+  local git_path = vim.fs.find('.git', {
+    upward = true,
+    path = search_path,
   })[1]
 
   if not git_path then
@@ -75,15 +88,23 @@ vim.keymap.set('n', '<leader>g', function()
   end
 
   local dir = vim.fs.dirname(git_path)
-  local Terminal = require('toggleterm.terminal').Terminal
-  local lazygit = Terminal:new {
-    cmd = 'lazygit',
-    hidden = true,
-    dir = dir,
-    direction = 'float',
-    float_opts = {
-      border = 'curved',
-    },
-  }
-  lazygit:toggle()
+
+  if not lazygit or lazygit.dir ~= dir then
+    if lazygit then
+      lazygit:shutdown()
+    end
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    lazygit = Terminal:new {
+      cmd = 'lazygit',
+      hidden = true,
+      dir = dir,
+      direction = 'float',
+      float_opts = {
+        border = 'curved',
+      },
+    }
+  end
+
+  lazygit:open()
 end, { desc = 'Open LazyGit' })
